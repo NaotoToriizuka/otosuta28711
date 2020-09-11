12e796b413c62c6daf342cb85d8ee725acb92469
NaotoToriizuka <toriizuka.com@gmail.co>
2020-09-11 19:24:45 +0900

    教室の登録、一覧、詳細、編集機能の修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,10 +5,12 @@ class RoomsController < ApplicationController
 
   def show
     @music = Music.find(params[:id])
+    @genres = @music.genres
   end
 
   def edit
     @music = Music.find(params[:id])
+    @genre = Genre.all
   end
 
 end
