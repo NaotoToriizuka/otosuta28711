1a23cf78d4c27f42293026144950f7bf3363fb82
NaotoToriizuka <toriizuka.com@gmail.co>
2020-09-18 15:37:00 +0900

    ログインの結合テストを実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe "ユーザー新規登録", type: :system do
   before do
     @user = FactoryBot.build(:user)
-    # driven_by(:rack_test)
   end
 
   context 'ユーザー新規登録ができる時' do
@@ -57,28 +56,45 @@ end
 
 RSpec.describe "ログイン", type: :system do
   before do
-    @user = FactoryBot.build(:user)
+    @user = FactoryBot.create(:user)
   end
   context 'ログインできるとき' do
     it '保存されているユーザーの情報と合致すればログインができる' do
       # トップページに移動する
+      visit root_path
       # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page). to have_content('ログイン')
       # ログインページへ遷移する
+      visit new_user_session_path
       # 正しいユーザー情報を入力する
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード(英数字6文字以上)', with: @user.password
       # ログインボタンを押す
+      find('input[name="commit"]').click
       # トップページへ遷移することを確認する
+      expect(current_path).to eq root_path
       # ログアウトボタンが表示されていることを確認する
+      expect(page).to have_content('ログアウト')
       # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
     end
   end
   context 'ログインできないとき' do
     it '保存されているユーザーの情報と合致しないとログインができない' do
       # トップページに移動する
+      visit root_path
       # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page). to have_content('ログイン')
       # ログインページへ遷移する
+      visit new_user_session_path
       # ユーザー情報を入力する
+      fill_in 'メールアドレス', with: ""
+      fill_in 'パスワード(英数字6文字以上)', with: ""
       # ログインボタンを押す
+      find('input[name="commit"]').click
       # ログインページへ戻されることを確認する
+      expect(current_path).to eq new_user_session_path
     end
   end
 end
